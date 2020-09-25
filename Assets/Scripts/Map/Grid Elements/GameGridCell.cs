bba9e3af00b7ccc2e4507da22079ca7cd5285572
Gonza Ferretti <gonzalofe1998@hotmail.com>
2020-09-25 06:57:23 -0300

    added grid indicators

--- Assets/Scripts/Map/Grid Elements/GameGridCell.cs
+++ Assets/Scripts/Map/Grid Elements/GameGridCell.cs
@@ -8,14 +8,6 @@ public class GameGridCell : GameGridElement
 {
     [SerializeField] Vector3Int coordinates;
 
-    [SerializeField] private Renderer rend;
-
-    [SerializeField] private Material selectedMaterial;
-    [SerializeField] private Material startMaterial;
-    [SerializeField] private Material endMaterial;
-    [SerializeField] private Material pathMaterial;
-    [SerializeField] private Material baseMaterial;
-    [SerializeField] private Material finalPathMaterial;
     public void SetCoordinates(Vector3Int coords)
     {
         coordinates = coords;
@@ -23,35 +15,5 @@ public class GameGridCell : GameGridElement
     public Vector3Int GetCoordinates()
     {
         return coordinates;
-    }
-
-    public void TintSelected()
-    {
-        rend.material = selectedMaterial;
-    }
-
-    public void TintStart()
-    {
-        rend.material = startMaterial;
-    }
-
-    public void TintEnd()
-    {
-        rend.material = endMaterial;
-    }
-
-    public void TintPath()
-    {
-        rend.material = pathMaterial;
-    }
-
-    public void TintFinalPath()
-    {
-        rend.material = finalPathMaterial;
-    }
-
-    public void Untint()
-    {
-        rend.material = baseMaterial;
     }
 }
---
+++ Assets/Scripts/Map/Grid/GridIndicator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridIndicator : MonoBehaviour
+{
+    [SerializeField] GridIndicatorMaterials availableMaterials;
+
+    [SerializeField] Renderer rend;
+
+    public void Disable()
+    {
+        rend.material = availableMaterials.GetMaterial(GridIndicatorMode.disabled);
+    }
+
+    public void Enable(GridIndicatorMode mode)
+    {
+        rend.material = availableMaterials.GetMaterial(mode);
+    }
+}
+
+public enum GridIndicatorMode
+{
+    disabled,
+    possibleMovement,
+    possibleAttack,
+    selectedAttack,
+    selectedMovement,
+}
