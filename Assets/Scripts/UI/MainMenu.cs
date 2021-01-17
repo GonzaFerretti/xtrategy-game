8219f27c286eec341a0b2f1aef879e134c222b6a
Gonza Ferretti <gonzalofe1998@hotmail.com>
2021-01-16 22:01:19 -0300

    Removed credit seperate scene and integrated it to mainmenu scene
    also added a simple menu change script

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    Dictionary<string, GameObject> possibleMenus;
+
+    [SerializeField] GameObject initialMenu;
+
+    public void LoadMenuDict()
+    {
+        possibleMenus = new Dictionary<string, GameObject>();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Transform childTrans = transform.GetChild(i);
+            possibleMenus.Add(childTrans.name, childTrans.gameObject);
+        }
+    }
+
+    public void LoadMenu(string name)
+    {
+        if (!possibleMenus.ContainsKey(name)) return;
+
+        foreach (var menu in possibleMenus)
+        {
+            menu.Value.SetActive(false);
+        }
+
+        possibleMenus[name].SetActive(true);
+    }
+
+    public void Start()
+    {
+        LoadMenuDict();
+        LoadMenu(initialMenu.name);
+    }
+}
