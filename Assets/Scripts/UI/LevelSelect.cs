6e013fd339f828cea850641f5807862851c13e76
Gonza Ferretti <gonzalofe1998@hotmail.com>
2021-01-16 23:17:52 -0300

    Level select for main menu

---
+++ Assets/Scripts/UI/LevelSelect.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    [SerializeField] float startingHeight;
+    [SerializeField] float padding;
+    [SerializeField] LevelSelectButton buttonPrefab;
+    [SerializeField] SaveManager saveManager;
+    [SerializeField] RectTransform backButton;
+
+    void Start()
+    {
+        int count = 0;
+        RectTransform rectTrans = GetComponent<RectTransform>();
+        for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
+        {
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            string nextScene = SceneUtility.GetScenePathByBuildIndex(i);
+            if (nextScene.Contains("Level"))
+            {
+                var newButton = Instantiate(buttonPrefab);
+                newButton.GetComponent<RectTransform>().parent = rectTrans;
+                string[] seperatedParts = nextScene.Replace(".unity", "").Split('/');
+                string cleanSceneName = seperatedParts[seperatedParts.Length - 1];
+
+                newButton.Setup(cleanSceneName, new Vector2(0,startingHeight - padding * count),saveManager);
+                count++;
+            }
+        }
+
+        float ySize = (count + 1) * padding + Mathf.Abs(startingHeight) * 2;
+        rectTrans.sizeDelta = new Vector2(rectTrans.sizeDelta.x, ySize);
+
+        backButton.anchoredPosition = new Vector2(0, startingHeight - padding * count);
+    }
+}
---
+++ Assets/Scripts/UI/LevelSelectButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelectButton : MonoBehaviour
+{
+    [SerializeField] Text text;
+    [SerializeField] Button button;
+    
+    public void Setup(string levelName, Vector2 position, SaveManager saveManager)
+    {
+        text.text = levelName;
+        gameObject.name = levelName;
+        RectTransform trans = GetComponent<RectTransform>();
+        trans.anchoredPosition = position;
+        trans.localScale = new Vector2(1, 1);
+        button.onClick.AddListener(() => saveManager.StageLoadForCleanLevel(levelName));
+    }
+}
